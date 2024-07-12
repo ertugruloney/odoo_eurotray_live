4aaca1a453e49e5118f3fff77602aa726bd9fb59
ertugruloney <ertugruloney96@gmail.com>
2024-07-12 22:54:42 +0300

    initial commit

--- odoo-17.0/addons/crm/models/sale_order.py
+++ odoo-17.0/addons/crm/models/sale_order.py
@@ -7,6 +7,6 @@ class SaleOrder(models.Model):
     crm_kart = fields.Many2one('crm.lead',string='New Field')
     try:
         partner_id = fields.Many2one(comodel_name='res.partner',
-            related='crm_kart.partner_id',readonly=True, string='Customer')
+            related='crm_kart.partner_id',readonly=True, string='Cusstomer')
     except:
         pass
